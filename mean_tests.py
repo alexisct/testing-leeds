63a62dff259d46d03f19e5ebf907ab95b78a2ba5
alexis cartwright-taylor <alexis.ct@gmail.com>
2016-06-30 16:27:55 +0100

    notes added to mean_tests

--- mean_tests.py
+++ mean_tests.py
@@ -1,17 +1,20 @@
 from mean import *
 
+# this tests that the code works with integers ok
 def test_ints():
     num_list = [1,2,3,4,5]
     obs = mean(num_list)
     exp = 3
     assert obs == exp
 
+# this tests that the code works with zeros in the input ok
 def test_zero():
     num_list=[0,2,4,6]
     obs = mean(num_list)
     exp = 3
     assert obs == exp
 
+# this tests that the code works with non-integers ok
 def test_double():
     # This one will fail in Python 2
     num_list=[1,2,3,4]
@@ -19,6 +22,7 @@ def test_double():
     exp = 2.5
     assert obs == exp
 
+# this tests that the code deals with large numbers ok
 def test_long():
     big = 100000000
     obs = mean(range(1,big))
